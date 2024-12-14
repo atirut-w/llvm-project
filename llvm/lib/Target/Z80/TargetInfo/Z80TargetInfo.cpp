e91b0a179c1309b0e5623a143c0ed2f155bcb266
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-12-14 17:13:53 +0700

    Register Z80 target

--- llvm/lib/Target/Z80/TargetInfo/Z80TargetInfo.cpp
+++ llvm/lib/Target/Z80/TargetInfo/Z80TargetInfo.cpp
@@ -1,3 +1,15 @@
+#include "llvm/MC/TargetRegistry.h"
 #include "llvm/Support/Compiler.h"
+#include "llvm/TargetParser/Triple.h"
 
-extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeZ80TargetInfo() {}
+namespace llvm {
+Target &getTheZ80Target() {
+  static Target TheZ80Target;
+  return TheZ80Target;
+}
+} // namespace llvm
+
+extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeZ80TargetInfo() {
+  llvm::RegisterTarget<llvm::Triple::z80> X(llvm::getTheZ80Target(), "z80",
+                                            "Zilog Z80", "Z80");
+}
---
+++ llvm/lib/Target/Z80/TargetInfo/Z80TargetInfo.h
@@ -0,0 +1,10 @@
+#ifndef LLVM_Z80_TARGET_INFO_H
+#define LLVM_Z80_TARGET_INFO_H
+
+namespace llvm {
+class Target;
+
+Target &getTheZ80Target();
+} // namespace llvm
+
+#endif // LLVM_Z80_TARGET_INFO_H
