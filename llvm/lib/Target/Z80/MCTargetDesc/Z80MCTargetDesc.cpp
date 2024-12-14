e3ce0a86bc77ed20f2bb2dd1edace613ae164b06
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-12-14 19:42:52 +0700

    Add tablegen stubs

--- llvm/lib/Target/Z80/MCTargetDesc/Z80MCTargetDesc.cpp
+++ llvm/lib/Target/Z80/MCTargetDesc/Z80MCTargetDesc.cpp
@@ -1,3 +1,17 @@
+#include "llvm/MC/MCRegisterInfo.h"
 #include "llvm/Support/Compiler.h"
+#include "llvm/TargetParser/Triple.h"
+
+#define GET_REGINFO_MC_DESC
+#include "Z80GenRegisterInfo.inc"
+
+using namespace llvm;
+
+static MCRegisterInfo *createZ80MCRegisterInfo(const Triple &TT) {
+  MCRegisterInfo *X = new MCRegisterInfo();
+  InitZ80MCRegisterInfo(X, 0);
+
+  return X;
+}
 
 extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeZ80TargetMC() {}
